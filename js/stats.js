4178b4de5185a9732428dc5d07ff023a54a5cd69
Roman Boiar <noovaroman@gmail.com>
2017-11-28 23:01:25 +0100

    task3

--- js/stats.js
+++ js/stats.js
@@ -1,44 +1,59 @@
 'use strict';
 
-window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.strokeRect(110, 20, 420, 270);
-  ctx.fillRect(110, 20, 420, 270);
+function getMaximum(array) {
+    var maximum = array[0];
 
-  ctx.fillStyle = 'white';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
-
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
-  
-   var max = -1;
-  var maxIndex = -1;
-
-  for (var i = 0 ; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-      maxIndex = i;
+    for (var i = 0; i < array.length; i++) {
+        maximum = Math.max(maximum, array[i]);
     }
-  }
-  
-  var histogramWidth = 150;              // px;
-  var step = histogramWidth / (max - 0); // px;
-  
-  ctx.fillText('Худшее время: ' + max.toFixed(0) + 'мс у игрока ' + names[maxIndex], 120, 80);
-  
-  var barHeigth = 20; // px; 
-  var indent = 40;    // px;
-  var initialX = 120; // px;
-  var initialY = 100;  // px;
-  var lineHeight = 15;// px;
-  
-  for(var i = 0; i < times.length; i++) {
-    ctx.fillRect(initialX, initialY + indent * i, times[i] * step, barHeigth);
-    ctx.fillText(names[i], initialX + histogramWidth, initialY + lineHeight + indent * i);
-  }
+    return maximum;
+}
+
+window.renderStatistics = function(ctx, names, times) {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.strokeRect(110, 20, 420, 270);
+    ctx.fillRect(110, 20, 420, 270);
+
+    ctx.fillStyle = 'white';
+    ctx.strokeRect(100, 10, 420, 270);
+    ctx.fillRect(100, 10, 420, 270);
+
+    ctx.fillStyle = '#000';
+    ctx.font = '16px PT Mono';
+
+    ctx.fillText('Ура вы победили!', 120, 40);
+    ctx.fillText('Список результатов:', 120, 60);
+
+    var initialX = 150;
+    var initialY = 100;
+    var width = 40;
+    var indent = 90;
+
+    var maxTime = getMaximum(times);
+
+    for (var i = 0; i < times.length; i++) {
+        if (names[i] === 'Вы') {
+            ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+        } else {
+            ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+        }
+        var heigth = times[i] / maxTime * 150;
+        ctx.fillRect(
+            initialX + indent * i,
+            initialY - heigth + 150,
+            width,
+            heigth
+        );
+        ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+        ctx.fillText(
+            names[i],
+            initialX + indent * i,
+            initialY + 165
+        );
+        ctx.fillText(
+            times[i].toFixed(0),
+            initialX + indent * i,
+            (initialY - 10) - heigth + 150
+        );
+    }
 };
